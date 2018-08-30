591fa24deb474f35918a67ef2e7dc5393f47abf7
anne-other <anne.c.ince@gmail.com>
2018-08-30 14:39:02 +0100

    adding the murolistview

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const Munros = require('./models/munros.js');
+const MunrosListView = require('./views/munros_list_view')
 
 document.addEventListener('DOMContentLoaded', () => {
+
+const munrosListContainer = document.querySelector('#munros')
+const munrosListView = new MunrosListView(munrosListContainer);
+munrosListView.bindEvents();
 
 const munros = new Munros('https://munroapi.herokuapp.com/api/munros');
 munros.getData();
